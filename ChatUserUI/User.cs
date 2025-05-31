a49e584aadcc7f58d18018946af0b4702281c759
SDH4114 <mammadov.amin2000@gmail.com>
2025-06-01 00:28:46 +0400

    startHtmlCN

--- ChatUserUI/User.cs
+++
@@ -1,9 +0,0 @@
-namespace ChatUserUI
-{
-    public class User
-    {
-        public string Nick { get; set; } = "";
-        public string Id { get; set; } = "";
-        public string Password { get; set; } = "";
-    }
-}
